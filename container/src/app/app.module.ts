d27ebc944bbad2a0a6637c807bb98351e0b9ace4
Harshal shirke <shirke.gorakh@igtsolutions.com>
2022-07-15 19:06:51 +0530

    Harshal :- Rounting and component done.In all microfrontned

--- container/src/app/app.module.ts
+++ container/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
