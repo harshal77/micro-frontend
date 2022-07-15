5600f0c70c3b3092ad77a411eff4ef515fcf1ed6
Harshal shirke <shirke.gorakh@igtsolutions.com>
2022-07-15 19:22:18 +0530

    Harshal :- Module federation added. Routing added.

--- container/src/app/app-routing.module.ts
+++ container/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -5,6 +6,26 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, pathMatch: 'full'},
+  {
+    path: 'mfe-2-student',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4202/remoteEntry.js',
+        exposedModule: './Student'
+      })
+        .then((m: any) => m.StudentModule)
+  },
+  {
+    path: 'mfe-2-employee',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4202/remoteEntry.js',
+        exposedModule: './Employee'
+      })
+        .then((m: any) => m.EmployeeModule)
+  },
   { path: '**', component: PageNotFoundComponent}
 ];
 
